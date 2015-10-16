594aa38b8e282c18727c2b6a7b37bb4e366db36b
Thomas Francis Shannon <Thomas@Thomas-MacBook-Pro.local>
2015-10-16 16:03:32 -0700

    added some functions etc.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+$("#answer1").html(avgPrice);
 
 
 
@@ -18,7 +19,7 @@ $(document).ready(function(){
 });
 
 var totalPrice = items.reduce(function(a, b) {                  //calculates average price
-	console.log(typeof a)
+
 	if (typeof a === "number") {
 		return a + b.price;
 	} else {
@@ -37,7 +38,7 @@ var avgPrice = (totalPrice/items.length).toFixed(2); 				///////
 
 
 
-var priceArr = items.filter(function(btwnObj){ 						//shows items between 14-18
+priceArr = items.filter(function(btwnObj){ 						//shows items between 14-18
 	if(btwnObj.price>14 && btwnObj.price<18) {
 		return true;
 	} else {
@@ -52,8 +53,12 @@ var newArray = priceArr.map(function(obj) {
 		title: obj.title,
 		 price: obj.price
 	}
-});																	///////
+});		
 
+newArray.forEach(function(obj){
+	$("#answer2").append("Title:").append(obj.title).append("Price:").append(obj.price);
+});
+															///////
 
 
 
@@ -77,7 +82,9 @@ var array = gbpArr.map(function(obj) {
 	}
 });																	//////////
 
-
+array.forEach(function(obj){
+	$("#answer3").append("Title:").append(obj.title).append("Price:").append(obj.price);
+});
 
 
 
@@ -86,9 +93,59 @@ var array = gbpArr.map(function(obj) {
 		title:obj.title,
 		materials:obj.materials
 	}
+});	
+
+var test=items.filter(function(obj){
+	for(i=0; i<matArray.length; i++) {
+	if(matArray[i]==="wood") {
+		return true;
+	} else {
+		return false
+	}
+}
+});*/
+ 
+ var newArr = items.filter(function(item) {
+ 	var isWood=false;
+ 	item.materials.forEach(function(material){
+ 		if(material === "wood") {
+ 			isWood = true;
+ 		}
+ 	 
+ 	 });
+ 	return isWood;
+ });
 
 
+
+
+
+
+
+
+var moreFilter= items.filter(function(obj) {				//8 or more
+	if(obj.materials.length>=8)
+		 {
+		 	return true;
+		 }
+});
+
+var test= moreFilter.map(function(obj){
+	 return {'title':obj.title,'materials':obj.materials,'materialLength':obj.materials.length};
+});
+
+test.forEach(function(obj){
+	$("#answer5").append("Title:").append(obj.title).append("Price:").append(obj.price);
+});										//
+
+
+var sellerFilter = items.filter(function(obj){				//who_made
+	if(obj.who_made==="i_did") {
+		return true;
+	} else {
+		return false;
+	}
 });
 
 
-console.log(matArray); */
+
